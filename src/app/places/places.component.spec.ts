a0594d08785ffacf7591cd7591521d45bd1ec955
Tony Fletcher <tfletch55@gmail.com>
2021-02-28 14:58:39 -0500

    Updated to latest Angular version

--- src/app/places/places.component.spec.ts
+++ src/app/places/places.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PlacesComponent } from './places.component';
 
@@ -6,7 +6,7 @@ describe('PlacesComponent', () => {
   let component: PlacesComponent;
   let fixture: ComponentFixture<PlacesComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PlacesComponent ]
     })
--- src/app/surveys/surveys.component.spec.ts
+++ src/app/surveys/surveys.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SurveysComponent } from './surveys.component';
 
@@ -6,7 +6,7 @@ describe('SurveysComponent', () => {
   let component: SurveysComponent;
   let fixture: ComponentFixture<SurveysComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SurveysComponent ]
     })
